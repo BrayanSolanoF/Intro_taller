566333e4aafbd8aa5d925feedb694fb34b05e4a9
BrayanSolanoF <36710897+BrayanSolanoF@users.noreply.github.com>
2018-03-21 01:19:33 -0600

    Add files via upload

---
+++ Recursion de Pila/Listas/Suma_raiz.py
@@ -0,0 +1,11 @@
+#Sumar las raices de los numero de una lista
+def raiz(lista):
+     if isinstance(lista,list):
+          return r(lista)
+     else:
+          return "Error"
+def r(lista):
+     if lista==[]:
+          return 0
+     else:
+         return lista[0]**(1/2)+r(lista[1:])
